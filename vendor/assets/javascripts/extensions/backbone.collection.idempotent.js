b6a8d5710d0b9b7d77ec29637154ed9e2785a056
Dennis Reimann <mail@dennisbloete.de>
2012-02-23 18:32:32 +0100

    Make it work with the current version of Backbone (0.9.1)

--- vendor/assets/javascripts/extensions/backbone.collection.idempotent.js
+++ vendor/assets/javascripts/extensions/backbone.collection.idempotent.js
@@ -1,11 +1,8 @@
 Backbone.Collection.prototype._addWithIdCheck = function(model, options) {
-  var idAttribute = model.idAttribute || this.model.prototype.idAttribute;
-  var modelId = model[idAttribute];
-
-  if (modelId === undefined || this.get(modelId) === undefined) {
+  if (model.id === undefined || this.get(model.id) === undefined) {
     this._addWithoutIdCheck(model, options);
   }
 };
 
-Backbone.Collection.prototype._addWithoutIdCheck = Backbone.Collection.prototype._add;
-Backbone.Collection.prototype._add = Backbone.Collection.prototype._addWithIdCheck;
+Backbone.Collection.prototype._addWithoutIdCheck = Backbone.Collection.prototype.add;
+Backbone.Collection.prototype.add = Backbone.Collection.prototype._addWithIdCheck;
